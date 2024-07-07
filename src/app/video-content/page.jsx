0b572f918a6cb3bd72004b441a03b8a9bc33246f
md-ali-0 <mohammad.98482@gmail.com>
2024-07-07 22:13:39 +0600

    Service Product Video Content Page Created

--- src/app/video-content/page.jsx
+++ src/app/video-content/page.jsx
@@ -1,5 +1,52 @@
+import bannerBd1 from "@/assets/banners/video-content/1.jpg";
+import bannerBd2 from "@/assets/banners/video-content/2.jpg";
+import bannerBd3 from "@/assets/banners/video-content/3.jpg";
+import bannerBd4 from "@/assets/banners/video-content/4.jpg";
+import bannerBd5 from "@/assets/banners/video-content/5.jpg";
+import CommonSlider from "@/components/common-slider";
+import EquipmentCard from "@/components/equipment-card";
+import SectionTitle from "@/components/section-title";
+import WhyChooseUs from "@/components/why-choose-us";
+
 const page = () => {
-    return <div></div>;
+    const ImageData = [
+        { image: bannerBd1 },
+        { image: bannerBd2 },
+        { image: bannerBd3 },
+        { image: bannerBd4 },
+        { image: bannerBd5 },
+    ];
+    return (
+        <div>
+            <CommonSlider>
+                {ImageData.map((item, idx) => (
+                    <div
+                        key={idx}
+                        style={{
+                            backgroundImage: `url('${item.image.src}')`,
+                        }}
+                        className="keen-slider__slide bg-cover bg-no-repeat bg-center flex items-center justify-center text-white"
+                    >
+                        <div className="bg-[rgba(255, 255, 255, 1)] flex flex-col justify-end backdrop-blur-[8px] rounded-3xl min-w-[80%] md:min-w-[380px] lg:min-w-[580px]">
+                            <div className="px-5 py-10 lg:px-14 lg:py-20">
+                                <h2 className="text-2xl font-bold mb-2">
+                                    Product Video Content
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </CommonSlider>
+            <div>
+                <SectionTitle
+                    heading={"Product Video Content"}
+                    subheading={`Showcase your products in the best light with our professional video equipment and expert guidance.`}
+                />
+                <WhyChooseUs />
+                <EquipmentCard />
+            </div>
+        </div>
+    );
 };
 
 export default page;
